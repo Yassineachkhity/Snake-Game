d715741712ce85078e74d627b0f5c03ff9e7e450
Yassine Achkhity <125568814+Yassineachkhity@users.noreply.github.com>
2024-04-21 12:58:53 +0200

    Add files via upload

---
+++ Snake/src/game/GameFrame.java
@@ -0,0 +1,18 @@
+package game;
+
+import javax.swing.JFrame;
+
+public class GameFrame extends JFrame{
+	
+	GameFrame(){
+		
+		this.add(new GamePanel());
+		this.setTitle("Snake");
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setResizable(false);
+		this.setVisible(true);
+		this.pack();
+		this.setLocationRelativeTo(null);
+	}
+
+}
---
+++ Snake/src/game/SnakeGame.java
@@ -0,0 +1,11 @@
+package game;
+
+public class SnakeGame {
+
+	public static void main(String[] args) {
+		
+		new GameFrame();
+
+	}
+
+}
